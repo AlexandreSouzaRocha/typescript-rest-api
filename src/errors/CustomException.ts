4d41025dc97008d52b08afda7f75903a754110ce
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2020-07-12 22:15:52 -0300

    src/errors/*: Configuring custom exception call

---
+++ src/errors/CustomException.ts
@@ -0,0 +1,16 @@
+import Constants from '../utils/constants';
+
+class CustomException extends Error {
+    message!: string;
+    exceptionType!: string;
+    statusCode!: number;
+
+    constructor(message: string, exceptionType: string, statusCode: number) {
+        super();
+        this.message = message || Constants.MESSAGE.DEFUALT.BAD_REQUEST;
+        this.exceptionType = exceptionType || Constants.EXCEPTION.CANDIDATE;
+        this.statusCode = statusCode || Constants.HTTPSTATUS.BAD_REQUEST;
+    }
+}
+
+export default CustomException;
