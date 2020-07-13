f1a379ff8e2109e8f4250d3e8c51f5b1b23be36d
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2020-07-12 22:16:22 -0300

    src/interfaces/*: Adding Candidate interface

---
+++ src/interfaces/Candidate.ts
@@ -0,0 +1,19 @@
+interface Candidate {
+    id?: string,
+    name: string,
+    birthDate: string,
+    rg: string,
+    cpf: string,
+    motherName: string,
+    fatherName: string,
+    address: string,
+    neighborhood: string,
+    zipCode: string,
+    country: string,
+    mobileNumber: string,
+    phoneNumber: string,
+    schooling: string,
+    schoolName: string,
+};
+
+export default Candidate;
