d36cdf4ad176fcdc88ecfacaf4136421d934b44e
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2020-08-31 21:43:53 -0300

    Adding configuration for global scope on typescript

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,15 @@ import { PORT } from './config';
 import Connection from './sequelize';
 import logger from './utils/logger';
 import Commons from './utils/Commons';
+import { dbpassword } from './utils/credentialsMiddleware';
 
 (async () => {
 	try {
 		const password: any = await Commons.getPassword();
 		const connection: Sequelize = await Connection.getInstance().getConnection(String(password));
 		await connection.sync();
-
-		logger.info({ event: '[DATABASE]', message: 'Connected on Postgres' });
+		global.dbPassword = password;
+		logger.info({ event: '[DATABASE]', message: 'Connected on Postgres', global: global.dbPassword });
 	} catch (err) {
 		logger.error({ event: '[DATABASE_ERROR]', error: err.message });
 	}
